f34345ad9053f6b6a466524a67b42ea02e5beb71
Aidan Torrence <aidan.torrence@gmail.com>
2021-08-14 09:59:48 -0400

    chat functionality

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react'
 import { View, StyleSheet, Text } from 'react-native';
-import CustomButton from '../components/CustomButton';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 export default function Chat({ navigation }) {
+  const [messages, setMessages] = useState([]);
+ 
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
   return (
-    <View style={styles.container}>
-        <Text>Chat</Text>
-    </View>
-  );
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-  },
-  button: {
-    borderRadius: 10,
-    padding: 20,
-  },
-  mainButton: {
-    backgroundColor: 'gray',
   },
 });
