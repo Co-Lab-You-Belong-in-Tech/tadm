cf6da4bcbcec64d2b5f3df08f936cff2118de34d
Aidan Torrence <aidan.torrence@gmail.com>
2021-08-14 11:47:12 -0400

    deleted unused

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -41,21 +41,6 @@ export default function Chat({ navigation }) {
     });
     return unsubscribe;
   }, []);
- 
-  // useEffect(() => {
-  //   setMessages([
-  //     {
-  //       _id: 1,
-  //       text: 'Hello developer',
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 2,
-  //         name: 'React Native',
-  //         avatar: 'https://placeimg.com/140/140/any',
-  //       },
-  //     },
-  //   ])
-  // }, [])
 
   const onSend = useCallback((messages = []) => {
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
